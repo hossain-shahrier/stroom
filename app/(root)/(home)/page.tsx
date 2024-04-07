ca51d2208b1b23c203dd6cc47ad6171dcd95bed5
Shahrier Hossain <hossainshahrier.shm@gmail.com>
2024-04-07 17:17:50 +0600

    home page banner updated

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,15 +1,29 @@
+'use client';
+
 import MeetingTypeList from '@/components/meeting-type-list/MeetingTypeList';
 import RunningTimeAndDate from '@/components/time-date/RunningTimeAndDate';
+import { useGetCalls } from '@/hooks/useGetCalls';
+import { Call, CallRecording } from '@stream-io/video-react-sdk';
 import React from 'react';
 
 const Home = () => {
+  const calls = useGetCalls();
+
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[270px] rounded-full py-1 text-center text-base font-normal">
-            Upoming Meeting at: 12.30px
-          </h2>
+          {calls.upcomingCalls && calls.upcomingCalls.length > 0 ? (
+            <p className="glassmorphism max-w-[400px] rounded-full py-1 text-center text-base font-normal">
+              Upcoming Meeting at:{' '}
+              {calls.upcomingCalls[0].state.startsAt?.toLocaleString()}
+            </p>
+          ) : (
+            <p className="glassmorphism max-w-[250px] rounded-full py-1 text-center text-base font-normal">
+              No upcoming meeting
+            </p>
+          )}
+
           <RunningTimeAndDate />
         </div>
       </div>
