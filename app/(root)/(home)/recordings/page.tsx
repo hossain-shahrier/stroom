d57dc26457f1e772083f9ccaab744cb159ea81a4
Shahrier Hossain <hossainshahrier.shm@gmail.com>
2024-04-07 16:25:01 +0600

    Change in recording

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -3,7 +3,7 @@ import CallList from '@/components/calllist/CallList';
 const PreviousPage = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Recordings</h1>
+      <h1 className="text-3xl font-bold">All Recordings</h1>
 
       <CallList type="recordings" />
     </section>
